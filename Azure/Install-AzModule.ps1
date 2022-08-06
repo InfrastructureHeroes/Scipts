cdf19f9869e31fa3f459b82052a1f9d575de50ae
Fabian Niesen <mail@fabian-niesen.de>
2022-08-06 20:42:14 +0200

    Create Install-AzModule.ps1

---
+++ Azure/Install-AzModule.ps1
@@ -0,0 +1,10 @@
+﻿Write-Verbose "Check for Admin"
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+If (!(Get-PackageProvider NuGet -ErrorAction SilentlyContinue).count -ge 1 ) { Install-PackageProvider -Name NuGet -Force -Confirm:$false } ELSE { Write-Output "NuGet Provider already configured"}
+If (!(get-module PowerShellGet -ErrorAction SilentlyContinue).count -ge 1 ) { Install-Module PowerShellGet -Force -Confirm:$false } ELSE { Write-Output "PowershellGet already installed"}
+If (!(get-command Connect-AzAccount -ErrorAction SilentlyContinue).count -ge 1 ) { Install-Module Az -Force -Confirm:$false } ELSE { Write-Output "Az Module already installed"}
